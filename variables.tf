6eb8bbe44decc38215cf1c92e65f672eb2aec1b9
vmalinics <vitalijs.malinics@managemy.com>
2021-12-07 15:46:25 +0000

    var conditional fix

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 #regex snapshots bucket name
 locals {
-  snapshots_events_topic_arn = var.create_snapshots_events_topic ? aws_sns_topic.rdsSnapshotsEvents[0].arn : var.snapshots_events_topic_arn
-  notifications_topic_arn    = var.create_notifications_topic ? aws_sns_topic.exportMonitorNotifications[0].arn : var.notifications_topic_arn
+  snapshots_events_topic_arn = var.enabled ? var.create_snapshots_events_topic ? aws_sns_topic.rdsSnapshotsEvents[0].arn : var.snapshots_events_topic_arn : ""
+  notifications_topic_arn    = var.enabled ? var.create_notifications_topic ? aws_sns_topic.exportMonitorNotifications[0].arn : var.notifications_topic_arn : ""
   kms_key_arn                = var.create_customer_kms_key ? aws_kms_key.snapshotExportEncryptionKey[0].arn : var.customer_kms_key_arn
   snapshots_bucket_arn       = "arn:aws:s3:::${var.snapshots_bucket_name}"
   prefix                     = var.prefix != null ? "${var.prefix}-" : ""
