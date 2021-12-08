cf8c363cf67b216dd74c79dc4bd923d432304f1b
vmalinics <vitalijs.malinics@managemy.com>
2021-12-08 11:11:59 +0000

    policy fix

--- kms.tf
+++ kms.tf
@@ -1,57 +1,85 @@
 #
+# KMS Policy 
+#
+data "aws_iam_policy_document" "snapshotExportEncryptionKeyPolicy" {
+  count = var.create_customer_kms_key ? 1 : 0
+
+  statement {
+    sid = "Allow administration of the key to the account"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions = [
+      "kms:*"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    sid = "Allow usage of the key"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${module.start_export_task_lambda.lambda_role_arn}"]
+    }
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+
+  statement {
+    sid = "Allow grants on the key"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${module.start_export_task_lambda.lambda_role_arn}"]
+    }
+
+    actions = [
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant"
+    ]
+
+    resources = [
+      "*",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+
+      values = [
+        "true"
+      ]
+    }
+  }
+}
+
+#
 # This key will be used for encrypting snapshots exported to S3
 #
 resource "aws_kms_key" "snapshotExportEncryptionKey" {
   count       = var.create_customer_kms_key ? 1 : 0
   description = "Snapshot Export Encryption Key"
   tags        = merge({ Name = "${local.prefix}kms-rds-snapshot-key${local.postfix}" }, var.tags)
-  policy      = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "Allow administration of the key to the account",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-            },
-            "Action": "kms:*",
-            "Resource": "*"
-        },
-        {
-            "Sid": "Allow usage of the key",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "${module.start_export_task_lambda.lambda_role_arn}"
-            },
-            "Action": [
-                "kms:Encrypt",
-                "kms:Decrypt",
-                "kms:ReEncrypt*",
-                "kms:GenerateDataKey*",
-                "kms:DescribeKey"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Sid": "Allow grants on the key",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "${module.start_export_task_lambda.lambda_role_arn}"
-            },
-            "Action": [
-                "kms:CreateGrant",
-                "kms:ListGrants",
-                "kms:RevokeGrant"
-            ],
-            "Resource": "*",
-            "Condition": {
-                "Bool": { "kms:GrantIsForAWSResource": "true" }
-            }
-        }
-    ]
-}
-POLICY
+  policy      = var.enabled ? data.aws_iam_policy_document.snapshotExportEncryptionKeyPolicy.json : null
 }
 
 #
